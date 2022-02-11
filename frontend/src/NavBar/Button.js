f4aac9a75f445b5e17761beaf51f6e848812ea66
Gabriel Rodriguez <GabeCreaCS@gmail.com>
2022-02-11 10:46:54 -0800

    Default CSS added

---
+++ frontend/src/NavBar/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Button.css';
+import { Link } from 'react-router-dom';
+
+
+export function Button(){
+    return (
+       <Link to='Login'>
+           <button className='btn'> Login </button>
+       </Link>
+    );
+}
+
+export default Button;
---
+++ frontend/src/NavBar/MenuItems.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const MenuItems = [
+{
+    title: 'HassleFree',
+    path: '/HassleFree',
+    cName: 'dropdown-link'
+},
+{
+    title: '24/7 Roadside Assistance',
+    path: '/RoadSide-Assistance',
+    cName: 'dropdown-link'
+},
+{
+    title: 'Nationwide Warranty',
+    path: '/NationWide-Warranty',
+    cName: 'dropdown-link'
+},
+{
+    title: '$250 Promise',
+    path: '/250-Promise',
+    cName: 'dropdown-link'
+},
+
+]
+
