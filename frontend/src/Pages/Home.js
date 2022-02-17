8108c4bb6136c830a57e10b2f311db646acaf444
Gabriel Rodriguez <GabeCreaCS@gmail.com>
2022-02-17 01:28:02 -0800

    Font Change added text to home page

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function Home() {
     return (
-        <div> Home page info </div>
+        <div> <p>AuTKonnect konnects you to trusted service providers, specialists
+        and businesses serving the autism & special needs community </p>
+        <p>
+        We provide resources, education and community.
+        </p>
+        <h2>Did you know?</h2>
+        <li>1 in 44 Autism</li>
+        <li>1 in 12 Communication Disorder</li>
+        <li>1 in 6 Sensory Processing Disorder</li>
+        </div>
+
+        
     )
 }
 
